4e3fcb8e9945944ab2d7c9f304b6139bc255a9e7
DavidTorres <dadyotorres1403@gmail.com>
2022-10-20 12:26:17 -0500

    new Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from "./components/Home";
 import { DataContextProvider } from "./components/contexts/DataContext";
 
+
 const App = () => {
     return (
         <DataContextProvider>
